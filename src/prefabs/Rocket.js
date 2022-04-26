b7355cc9f39fd6555842669f61e6419bc9ba075c
nstepp2001 <nstepp@ucsc.edu>
2022-04-25 18:01:06 -0700

    the rocket is zooming

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,7 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite{
         //add object to existing scene
         scene.add.existing(this);
         this.isFiring = false;
-        this.moveSpeed = 2;
+        this.moveSpeed = 10;
         this.sfxRocket = scene.sound.add('sfx_rango');
     }
 
